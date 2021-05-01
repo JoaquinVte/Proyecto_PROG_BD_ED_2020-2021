22f6d9386894ae48ed84d680ce25aa652142eefe
Joaquin Vte. Alonso Saiz <joaalsai@gmail.com>
2021-05-01 17:42:28 +0200

    Añadidas las propiedades seguras

--- src/com/mordor/lloguer/model/MyDataSource.java
+++ src/com/mordor/lloguer/model/MyDataSource.java
@@ -1,41 +1,31 @@
 package com.mordor.lloguer.model;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Properties;
-
 import javax.sql.DataSource;
+
+import com.mordor.lloguer.config.MyConfig;
 
 import oracle.jdbc.pool.OracleDataSource;
 
 public class MyDataSource {
 
 	public static DataSource getOracleDataSource() {
-		
-		// Propiedades donde tenemos los datos de acceso a la BD
-		Properties props = new Properties();
-		
+
 		// Objeto DataSource que devolveremos
 		OracleDataSource oracleDS = null;
-		
-		try(FileInputStream fis = new FileInputStream("db.properties");) {
-			
-			// Cargamos las propiedades
-			props.load(fis);
-			
+
+		try {
+
 			// Generamos el DataSource con los datos URL, user y passwd necesarios
 			oracleDS = new OracleDataSource();
-			oracleDS.setURL(props.getProperty("ORACLE_DB_URL"));
-			oracleDS.setUser(props.getProperty("ORACLE_DB_USERNAME"));
-			oracleDS.setPassword(props.getProperty("ORACLE_DB_PASSWORD"));
-			
-		} catch (IOException e) {
-			e.printStackTrace();
+			oracleDS.setURL(MyConfig.getInstancia().getURL());
+			oracleDS.setUser(MyConfig.getInstancia().getUsername());
+			oracleDS.setPassword(MyConfig.getInstancia().getPassword());
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return oracleDS;
 	}
 }
